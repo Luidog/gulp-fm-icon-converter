9a9ab7d2c14f440a259097a1fa5ace1e669c7b03
Lui de la Parra <lui@mutesymphony.com>
2018-02-09 23:02:23 -0800

    Removes Unneccessary attributes
    This commit removes the unneccesary attributes such as title, description, and definitions.
    
    Signed-off-by: Lui de la Parra <lui@mutesymphony.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-'use strict'
+'use strict';
 
-const gulp = require('gulp')
-const cheerio = require('gulp-cheerio')
-const { folders, files, shapes } = require('./configuration/settings')
+const gulp = require('gulp');
+const cheerio = require('gulp-cheerio');
+const { folders, files, shapes, unusedAttributes } = require('./configuration');
 
 /**
  * fixElement removes any element attributes of a shape then adds the fm_fill class
@@ -10,25 +10,38 @@ const { folders, files, shapes } = require('./configuration/settings')
  * @param  {html} icon The icon to modify
  */
 const fixElement = ($, icon) => {
-  for (let shape of shapes) {
-    let element = $(shape)
-    element.attribs = {}
-    element.addClass('fm_fill').attr('fill', '#ccc')
+  for (let attribute of unusedAttributes) {
+    removeAttributes($, attribute);
   }
-}
+  for (let shape of shapes) {
+    let element = $(shape);
+    element.attribs = {};
+    element.addClass('fm_fill').attr('fill', '#ccc');
+  }
+};
+
+/**
+ * removeAttributes removes unused html tags of the icon.
+ * @param  {[type]} attribute The attribute that should be removed.
+ * @return {function}         removes the attribute
+ */
+const removeAttributes = ($, attribute) => $(attribute).remove();
 
 gulp.task('add-fm-class', () => {
-  console.log('icons converted - 🍺')
-  gulp.src(`${folders.source}/*.svg`)
-    .pipe(cheerio({
-      run: ($, icon) => fixElement($, icon)
-    }))
-    .pipe(gulp.dest(folders.destination))
-})
+  console.log('icons converted - 🍺');
+  gulp
+    .src(`${folders.source}/*.svg`)
+    .pipe(
+      cheerio({
+        run: ($, icon) => fixElement($, icon)
+      })
+    )
+    .pipe(gulp.dest(folders.destination));
+});
 
 gulp.task('watch', () => {
-  console.log(`👀   watching ./${folders.source} for icons. 👀`)
-  return gulp.watch(files, ['add-fm-class'])
-})
+  console.log(`👀   watching ./${folders.source} for icons. 👀`);
+  return gulp.watch(files, ['add-fm-class']);
+});
 
-gulp.task('default', ['add-fm-class', 'watch'])
+gulp.task('default', ['add-fm-class', 'watch']);
